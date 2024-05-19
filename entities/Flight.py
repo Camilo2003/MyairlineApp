0fc695ae5ef6330ca98d947554928285420f16c5
Camilo2003 <camilo.munoz2@utp.edu.co>
2024-05-19 12:51:46 -0500

    update

--- entities/Flight.py
+++ entities/Flight.py
@@ -2,29 +2,28 @@
 
 
 # Importing necessary classes
-from Airplane import Airplane
 from Passenger import Passenger
 
 # Flight class
 
 
 class Flight:
-    def __init__(self, id: int, airplane: Airplane, passengers: list[Passenger], departure: str, destination: str, departure_time: str, arrival_time: str, is_finished=False) -> None:
+    def __init__(self, id: int, id_airplane: int, passengers: list[Passenger], departure: str, destination: str, departure_time: str, arrival_time: str, status: str) -> None:
         self.__id = id
-        self.__airplane = airplane
+        self.__id_airplane = id_airplane
         self.__passengers = passengers
         self.__departure = departure
         self.__destination = destination
         self.__departure_time = departure_time
         self.__arrival_time = arrival_time
-        self.__is_finished = is_finished
+        self.__status = status
 
     # Getters
     def get_id(self):
         return self.__id
 
-    def get_airplane(self):
-        return self.__airplane
+    def get_id_airplane(self):
+        return self.__id_airplane
 
     def get_passengers(self):
         return self.__passengers
@@ -41,15 +40,15 @@ class Flight:
     def get_arrival_time(self):
         return self.__arrival_time
 
-    def is_finished(self):
-        return self.__is_finished
+    def get_status(self):
+        return self.__status
 
     # Setters
     def set_id(self, id):
         self.__id = id
 
-    def set_airplane(self, airplane):
-        self.__airplane = airplane
+    def set_id_airplane(self, id_airplane):
+        self.__id_airplane = id_airplane
 
     def set_departure(self, departure):
         self.__departure = departure
@@ -63,22 +62,23 @@ class Flight:
     def set_arrival_time(self, arrival_time):
         self.__arrival_time = arrival_time
 
-    def set_is_finished_true(self):
-        self.__is_finished = True
-
-    def set_is_finished_false(self):
-        self.__is_finished = False
+    def set_status(self, status):
+        self.__status = status
 
     # Methods
     def add_passenger(self, passenger):
         self.__passengers.append(passenger)
 
     def remove_passenger(self, id_passenger: int):
-        self.__passengers = [
-            passenger for passenger in self.__passengers if passenger.get_id() != id_passenger]
+        for passenger in self.__passengers:
+            if passenger.get_id() == id_passenger:
+                self.__passengers.remove(passenger)
 
     def get_info(self):
-        return f"Flight ID: {self.__id}, Departure: {self.__departure}, Destination: {self.__destination}, Departure Time: {self.__departure_time}, Arrival Time: {self.__arrival_time}, Finished: {self.__is_finished}"
+        return f"Flight ID: {self.__id}, Departure: {self.__departure}, Destination: {self.__destination}, Departure Time: {self.__departure_time}, Arrival Time: {self.__arrival_time},"
+
+    def get_info_and_status(self):
+        return f"Flight ID: {self.__id}, Departure: {self.__departure}, Destination: {self.__destination}, Departure Time: {self.__departure_time}, Arrival Time: {self.__arrival_time}, Status: {self.__status}"
 
     def get_passengers_info(self):
         list_info_passengers = []
@@ -87,30 +87,11 @@ class Flight:
         return list_info_passengers
 
 
-# Example
-# Airplane
-Airplane1 = Airplane(1, "Boeing", "747", 416)
-Airplane2 = Airplane(2, "Airbus", "A380", 853)
+# # examples
 
-# Passengers
-Passenger1 = Passenger(1, "John", "Doe", 1, 1)
-Passenger2 = Passenger(2, "Jane", "Doe", 1, 2)
-Passenger3 = Passenger(3, "Alice", "Doe", 2, 1)
-Passenger4 = Passenger(4, "Bob", "Doe", 2, 2)
-
-# Flight
-Flight1 = Flight(1, Airplane1, [],
-                 "New York", "Los Angeles", "08:00", "10:00")
-Flight2 = Flight(2, Airplane2, [],
-                 "Paris", "Tokyo", "12:00", "20:00")
-
-
-# Adding passengers to the flight
-Flight1.add_passenger(Passenger1)
-Flight1.add_passenger(Passenger2)
-Flight2.add_passenger(Passenger3)
-Flight2.add_passenger(Passenger4)
-
-# Getting passengers info
-list_passengers_info = Flight1.get_passengers_info()
-print(list_passengers_info)
+# flight1 = Flight(1, 1, [], "Bogota", "Medellin", "10:00", "12:00", "On Time")
+# print(flight1.get_info_and_status())
+# print(flight1.get_passengers_info())
+# passenger1 = Passenger(123456789, "Juan", "Perez", 1)
+# flight1.add_passenger(passenger1)
+# print(flight1.get_passengers_info())
--- entities/Passenger.py
+++ entities/Passenger.py
@@ -9,25 +9,18 @@ from Person import Person
 # Passenger class
 # Inheritance from Person class
 class Passenger(Person):
-    def __init__(self, document: int, first_name: str, last_name: str, id_flight: int, seat_number: int) -> None:
+    def __init__(self, document: int, first_name: str, last_name: str, id_flight: int) -> None:
         super().__init__(document, first_name, last_name)
         self.__id_flight = id_flight
-        self.__seat_number = seat_number
 
     # Getters
     def get_id_flight(self):
         return self.__id_flight
 
-    def get_seat_number(self):
-        return self.__seat_number
-
     # Setters
     def set_id_flight(self, id_flight):
         self.__id_flight = id_flight
 
-    def set_seat_number(self, seat_number):
-        self.__seat_number = seat_number
-
     # Methods
     def get_info(self):
-        return f"{super().get_info()}, ID Flight: {self.__id_flight}, Seat Number: {self.__seat_number}"
+        return f"{super().get_info()}, ID Flight: {self.__id_flight}"
